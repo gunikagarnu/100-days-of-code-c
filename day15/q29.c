7bca5765e0178a3476b6baacb11e0fd6646f56f2
gunikagarnu <Gunika.41052@stu.upes.ac.in>
2026-09-18 01:45:53 +0530

    Add factorial calculation program
    Implement a program to calculate the factorial of a number.

---
+++ day15/q29.c
@@ -0,0 +1,18 @@
+//Q29: Write a program to calculate the factorial of a number.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    long long factorial = 1; 
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+
+    printf("%lld\n", factorial);
+
+    return 0;
+}
